69d429e3eb8724112b6ae6e98e6680d8967d55f6
simonlou-dev <simon.lou.dev@proton.me>
2024-11-18 08:37:34 +0100

    End of rank calculation

--- app/services/rank_service.ts
+++ app/services/rank_service.ts
@@ -2,7 +2,7 @@ import User from '#models/user'
 import Group from '#models/group'
 import Game from '#models/game'
 import PartyTeam from '#models/party_team'
-import UserRank from "#models/user_rank";
+import UserRank from '#models/user_rank'
 
 export default class RankService {
   public static async getTeamRank(group: Group, game: Game): Promise<number> {
@@ -15,15 +15,33 @@ export default class RankService {
     return teamRank / group.members.length
   }
 
-  public static calculateWinProbabilityOfTeamA(teamA: PartyTeam, teamB: PartyTeam): number {
+  public static async calculateWinProbabilityOfTeamA(
+    teamA: PartyTeam,
+    teamB: PartyTeam
+  ): Promise<number> {
+    await teamA.load('players')
+    await teamA.load('party')
+    await teamA.party.load('mode')
+    await teamA.party.mode.load('game')
+    await teamB.load('players')
+    let teamARank = 0
+    let teamBRank = 0
+    teamA.players.map(async (member) => {
+      let rank = await this.getUserRank(member, teamA.party.mode.game)
+      teamARank *= Math.pow(10, rank.rank / 500)
+    })
+    teamB.players.map(async (member) => {
+      let rank = await this.getUserRank(member, teamA.party.mode.game)
+      teamBRank *= Math.pow(10, rank.rank / 500)
+    })
 
+    return teamARank / (teamARank + teamBRank)
   }
 
   public static async calculateRank(user: User, game: Game, score: number, winProbability: number) {
     await user.load('ranks')
     let rank = await this.getUserRank(user, game)
     let devCoef = 0
-
 
     if (rank.playedGames <= 5) devCoef = 70
     else if (rank.playedGames > 5 && rank.playedGames <= 30) devCoef = 40
